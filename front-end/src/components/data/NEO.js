48c5b4a7ad4bd39f3f47ee6ca8035aa0e2747c55
ErgoEuan <ergoeuan@gmail.com>
2021-03-26 23:28:20 +0000

    working version

--- front-end/src/components/data/NEO.js
+++ front-end/src/components/data/NEO.js
@@ -1,33 +1,11 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import {useFrame} from 'react-three-fiber';
 
 import {useGLTF, Sphere, Html} from 'drei';
 
+import {useSection} from '../utils'
+
 // import PropTypes from 'prop-types';
-
-// class NEO extends Component {
-
-//     render() {
-//         console.log(this.props.neo)
-//         return (
-//             <div>
-//                 ----------------<br/>
-//                 Apsolute Magitude: {this.props.neo.absolute_magnitude_h}<br/>
-//                 Estimated Diameter in km Max: {this.props.neo.estimated_diameter.kilometers.estimated_diameter_max}<br/>
-//                 Estimated Diameter in km Min: {this.props.neo.estimated_diameter.kilometers.estimated_diameter_min}<br/>
-//                 Estimated Diameter in m Max: {this.props.neo.estimated_diameter.meters.estimated_diameter_max}<br/>
-//                 Estimated Diameter in m Min: {this.props.neo.estimated_diameter.meters.estimated_diameter_min}<br/>
-//                 ID: {this.props.neo.id}<br/>
-//                 Potentially Hazardous: {this.props.neo.is_potentially_hazardous_asteroid ? 'true' : 'false'}<br/>
-//                 Sentry Object: {this.props.neo.is_sentry_object ? 'true' : 'false'}<br/>
-//                 Name: {this.props.neo.name}<br/>
-//                 JPL: {this.props.neo.nasa_jpl_url}<br/>
-//                 NEO Refference ID: {this.props.neo.neo_reference_id}<br/>
-
-//             </div>  
-//         );
-//     }
-// }
 
 const AsteroidModel = ({modelPath}) => {
     const gltf = useGLTF(modelPath, true);
@@ -35,13 +13,13 @@ const AsteroidModel = ({modelPath}) => {
 };
 
 const NEO = ({neo, count}) => {
-    console.log(count)
+    // console.log(count)
     const NumberModle = '1';
     const modelPath = '/asteroid' + NumberModle + '/scene.gltf';
 
     const size = ((neo.estimated_diameter.meters.estimated_diameter_max - neo.estimated_diameter.meters.estimated_diameter_min) / 2) + neo.estimated_diameter.meters.estimated_diameter_min;
 
-    console.log(size)
+    // console.log(size)
 
     var sizeTemp = 0;
     if (size <= 25) {
@@ -60,13 +38,42 @@ const NEO = ({neo, count}) => {
         sizeTemp = 40
     }
 
-    console.log(sizeTemp)
-
+    const width = Math.floor(useSection().canvasWidth) -20;
+    console.log(width)
+    // const xtemp = (width * (Math.sin(count) - count)) ;
+    const ytemp = Math.floor(Math.sin(count) * 5) + 2;
     const xtemp = (count - 7) * 3;
-    const ytemp = Math.floor(Math.random() * 11) + -2
+    // const xtemp = Math.floor((width/2) - width) + (width/count) ;
+    // const ytemp = Math.floor(Math.random() * 11) + -2;
 
     const mesh = useRef(null);
     useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+
+    //expand boxs
+
+    const [opacity, setOpacity] = useState("0")
+    const [position, setPosition] = useState("absolute")
+    const [zIndex, setZIndex] = useState("3")
+    const [zIndexB, setZIndexB] = useState("4")
+    
+
+    function activateExpand() {
+
+        if (opacity === "0") {
+            setOpacity("1")
+            setPosition("relative")
+            setZIndex("4")
+            setZIndexB("5")
+        }
+        else {
+            setOpacity("0")
+            setPosition("absolute")
+            setZIndex("3")
+            setZIndexB("2")
+        }
+        console.log('name clicked')
+    }
+
     return (
         <mesh castShadow position={[xtemp, ytemp, sizeTemp]} ref={mesh}>
             <AsteroidModel modelPath={modelPath}/>
@@ -74,7 +81,24 @@ const NEO = ({neo, count}) => {
                 <meshStandardMaterial attach='material' color="#323030"/>
             </Sphere>
             <Html>
-                <h3 style={{ color: "white"}}>{neo.name}</h3>
+                <div className="neo-data" style={{ zIndex: zIndex}}>
+                    <div className="neo-data-name" onClick={activateExpand}>
+                        <h3>{neo.name}</h3>
+                    </div>
+                    <div className="neo-data-expand" style={{ opacity: opacity, position: position, zIndex: zIndexB}}>
+                        Apsolute Magitude: {neo.absolute_magnitude_h}<br/>
+                        Estimated Diameter in km Max: {neo.estimated_diameter.kilometers.estimated_diameter_max}<br/>
+                        Estimated Diameter in km Min: {neo.estimated_diameter.kilometers.estimated_diameter_min}<br/>
+                        Estimated Diameter in m Max: {neo.estimated_diameter.meters.estimated_diameter_max}<br/>
+                        Estimated Diameter in m Min: {neo.estimated_diameter.meters.estimated_diameter_min}<br/>
+                        ID: {neo.id}<br/>
+                        Potentially Hazardous: {neo.is_potentially_hazardous_asteroid ? 'true' : 'false'}<br/>
+                        Sentry Object: {neo.is_sentry_object ? 'true' : 'false'}<br/>
+                        Name: {neo.name}<br/>
+                        JPL: {neo.nasa_jpl_url}<br/>
+                        NEO Refference ID: {neo.neo_reference_id}<br/>
+                    </div>  
+                </div>
             </Html>
         </mesh>
     );
--- front-end/src/components/utils.js
+++ front-end/src/components/utils.js
@@ -1,3 +1,6 @@
+import { createContext, useContext } from "react";
+import { useThree } from "react-three-fiber";
+
 export function getCurrentDate(separator='-'){
 
     let newDate = new Date()
@@ -14,4 +17,32 @@ export function getAPIKey(){
     let apiKey = 'AOb7pUI6SWcptJeaS13zybABmo1eQrCvQPTtOVzq'
 
     return `${apiKey}`
+}
+
+const offsetContext = createContext(0);
+
+export function useSection() {
+
+    const { size, viewport } = useThree();
+    const offset = useContext(offsetContext);
+    const viewportWidth = viewport.width;
+    const viewportHeight = viewport.height;
+    const canvasWidth = viewportWidth;
+    const canvasHeight = viewportHeight;
+    const mobile = size.width < 700;
+    const margin = canvasWidth * (mobile ? 0.2 : 0.1);
+    const contentMaxWidth = canvasWidth * (mobile ? 0.8 : 0.6);
+    const aspect = size.height / viewportHeight;
+    return {
+      aspect,
+      viewport,
+      offset,
+      viewportWidth,
+      viewportHeight,
+      canvasWidth,
+      canvasHeight,
+      mobile,
+      margin,
+      contentMaxWidth,
+    };
 }
